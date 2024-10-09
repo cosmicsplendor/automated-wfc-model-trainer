913e113eb2cb91a38d95ed918ed186514332f569
Umesh KC <ufocalypse@gmail.com>
2024-10-09 11:44:15 +0545

    Make it possible to train the model on multiple datasets

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ const { initializeGrid, initializeAdjacencyTable, populateGrid, updateAdjacencyT
 const tileWidth = 48
 const tileHeight = 48
 
-const tiles = data.fgTiles.concat(data.tiles)
-const maxY = tiles.reduce((maxY, tile) => Math.max(maxY, tile.y), 0)
-const gridOffset = -maxY % 48
-const validTiles = tiles.filter(tile => {
-    const valid = walltiles.includes(tile.name)
-    const snapped = tile.x % 48 === 0 && (tile.y + gridOffset) % 48 === 0
-    return valid && snapped
-})
-const minY = validTiles.reduce((y0, tile) => Math.min(y0, tile.y), Infinity)
-const width = validTiles.reduce((maxX, tile) => Math.max(tile.x, maxX), 0) + tileWidth
-validTiles.forEach(tile => {
-    tile.y -= minY
-})
-const height = maxY + tileHeight - minY
+const generateGrid = data => {
+    const tiles = data.fgTiles.concat(data.tiles)
+    const maxY = tiles.reduce((maxY, tile) => Math.max(maxY, tile.y), 0)
+    const gridOffset = -maxY % 48
+    const validTiles = tiles.filter(tile => {
+        const valid = walltiles.includes(tile.name)
+        const snapped = tile.x % 48 === 0 && (tile.y + gridOffset) % 48 === 0
+        return valid && snapped
+    })
+    const minY = validTiles.reduce((y0, tile) => Math.min(y0, tile.y), Infinity)
+    const width = validTiles.reduce((maxX, tile) => Math.max(tile.x, maxX), 0) + tileWidth
+    validTiles.forEach(tile => {
+        tile.y -= minY
+    })
+    const height = maxY + tileHeight - minY
+
+    const grid = initializeGrid(width, height, tileWidth, tileHeight)
+
+    populateGrid(grid, validTiles, tileWidth, tileHeight)
+    return grid
+    // fs.writeFileSync("./grid.json", JSON.stringify(grid))
+}
+
 const tileTypes = ["empty", ...walltiles]
-
-const grid = initializeGrid(width, height, tileWidth, tileHeight)
 const adjacencyTable = initializeAdjacencyTable(tileTypes)
-
-populateGrid(grid, validTiles, tileWidth, tileHeight)
-fs.writeFileSync("./grid.json", JSON.stringify(grid))
+const grid = generateGrid(data)
 updateAdjacencyTable(grid, adjacencyTable)
 exportAdjacencyTable("./adjacency.json", adjacencyTable)
